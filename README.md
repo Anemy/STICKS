22afbfece6dd2e1324afe12644018a754ff7bf84
Anemy <rhysh@live.com>
2014-01-24 12:38:19 -0500

    Made dirrectories for server client and updated readme

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ STICKS
 ======
 
 Paul + Rhys Stick Battle
+
+client in client and server in server
+
+All in javascript using html 5 canvas
+
+Plans to go online using node.js and Socket.io and maybe comet?
---
+++ server/server.js
@@ -0,0 +1,16 @@
+//Server test 1
+//Stick Battle
+//Rhys H and Paul H
+
+var sys = require('sys'),
+	http = require('http')
+
+http.createServer(function (req, res) {
+	setTimeout(function () {
+		res.sendHeader(200, {'Content-Type': 'text/plain'})
+		res.sendBody('Hello World')
+		res.finish()
+	}, 2000)
+}).listen(8000)
+
+sys.puts('Server running at http://127.0.0.1:8000/')
